33f41dae0d7e295257273f2f3d55955c3b251b98
Philip Jurke <pjurke@icloud.com>
2024-02-03 22:05:58 +0100

    add documentation link

---
+++ Personal Guide/Settings/Documentation/DocumentationLink.swift
@@ -0,0 +1,22 @@
+//
+//  DocumentationLink.swift
+//  Personal Guide
+//
+//  Created by Philip Jurke on 03.02.24.
+//
+
+import SwiftUI
+
+struct DocumentationLink: View {
+    
+    var body: some View {
+        
+        Link("Documentation", destination: URL(string: "https://personal-guide.gitbook.io/documentation/")!)
+        
+    }
+    
+}
+
+#Preview {
+    DocumentationLink()
+}
--- Personal Guide/Settings/SettingsView.swift
+++ Personal Guide/Settings/SettingsView.swift
@@ -14,6 +14,7 @@ struct SettingsView: View {
         Form {
             
             AppVersionSection()
+            DocumentationLink()
             
         }
         .navigationBarTitleDisplayMode(.inline)
