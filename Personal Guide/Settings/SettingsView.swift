38cfa8be0424e5044d391ee4c41c29ceec0245c7
Philip Jurke <pjurke@icloud.com>
2024-02-03 21:42:11 +0100

    add settings view and add versioning section

---
+++ Personal Guide/Settings/SettingsView.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsView.swift
+//  Personal Guide
+//
+//  Created by Philip Jurke on 03.02.24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    var body: some View {
+        
+        Form {
+            
+            AppVersioningSection()
+            
+        }
+        
+    }
+    
+}
+
+#Preview {
+    SettingsView()
+}
